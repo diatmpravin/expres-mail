0bb3df42629dd38f0dda1ae607eb7c92bccc33e7
Pravin Kumar <Pravin.Kumar@gmail.com>
2020-06-26 01:22:48 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+let express = require("express"),
+  path = require('path'),
+  nodeMailer = require('nodemailer'),
+  bodyParser = require('body-parser');
+
+let app = express();
+
+app.use(express.static('src'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.post('/send-email', function (req, res) {
+  let transporter = nodeMailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+          // should be replaced with real sender's account
+          user: 'xxxxxxxxxx@gmail.com',
+          pass: 'xxxxxxxx'
+      }
+  });
+  let mailOptions = {
+      // should be replaced with real recipient's account
+      to: 'xxxxxxxxxxx',
+      subject: req.body.subject,
+      body: req.body.message
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+  res.writeHead(301, { Location: 'index.html' });
+  res.end();
+});
+
+
+let server = app.listen(process.env.PORT || 5000, function(){
+    let port = server.address().port;
+    console.log("Server started at http://127.0.0.0:%s", port);
+});
